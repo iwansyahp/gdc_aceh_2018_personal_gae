9623df13113d30cf4f94a5b25597d28c77654e64
Iwansyah Putra <iwansyahp@gmail.com>
2018-07-29 12:57:32 +0700

    aplikasi sudah bisa menampilkan hello world

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<html>
+  <head>
+    <title>PHP runs on GAE</title>
+  </head>
+  <body>
+    <?php
+    echo "Tes, 1, 2, 3";
+    ?>
+    <h1>PHP is never cool! &#x1F60E;</h1>
+    <!-- START:Opsional -->
+    <img src="https://memegenerator.net/img/instances/67407853/not-sure-if-im-bad-at-php-or-php-is-shit.jpg" alt="I love PHP, but I am lying">
+    <!-- END:Opsional -->
+  </body>
+</html>
